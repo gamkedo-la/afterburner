6e53f448bcb12d33ff9d1c09b414d961612511d4
Chris DeLeon <gamkedo@gmail.com>
2016-12-18 02:50:20 -0800

    Alien mothership added to 2142 test/demo scene, prefab created. Only vulnerable on its angler fruit colorful things. Spawns self-destructing crystal aliens

---
+++ Assets/Scripts/Enemy/CrystalEnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalEnemySpawner : MonoBehaviour {
+	private EnemyHealth myHealth;
+	public GameObject enemyToSpawn;
+
+	IEnumerator EnemySpawn() {
+		while(myHealth.IsDead == false) {
+			yield return new WaitForSeconds(Random.Range(4.0f, 8.0f));
+			GameObject tempGO = (GameObject)GameObject.Instantiate(enemyToSpawn,
+				transform.position, transform.rotation);
+			SelfDestructTime sdtScript = tempGO.GetComponent<SelfDestructTime>();
+			sdtScript.enabled = true;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		myHealth = GetComponentInParent<EnemyHealth>();
+
+		StartCoroutine(EnemySpawn());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Enemy/GiantAlienSpinAI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GiantAlienSpinAI : MonoBehaviour {
+	EnemyHealth myHealth;
+	float rotY=0.0f;
+	float spinRate;
+
+	IEnumerator Rethink() {
+		while(myHealth.IsDead == false) {
+			bool wasNeg = (spinRate < 0.0f);
+			spinRate = Random.Range(25.0f,70.0f);
+			if(wasNeg == false) {
+				spinRate *= -1.0f;
+			}
+			yield return new WaitForSeconds(Random.Range(1.6f, 4.4f));
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		myHealth = GetComponent<EnemyHealth>();
+
+		if(Random.Range(0.0f, 1.0f) < 0.5f) {
+			spinRate = -1.0f;
+		} else {
+			spinRate = 1.0f;
+		}
+		StartCoroutine(Rethink());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotY += spinRate * Time.deltaTime;
+		transform.rotation =
+			Quaternion.AngleAxis(rotY, Vector3.up);
+	}
+}
