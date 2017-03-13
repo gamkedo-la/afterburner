cf195fbcc9a3c863880e1071ff7295866f955e48
NickPolchies <polchiesnicholas@gmail.com>
2017-03-13 02:04:53 -0300

    Future turret wreckage now correct size and orientation
    Future turret wreckage seems to rotate randomly
    It's not a bug, it's a feature

---
+++ Assets/Scripts/Enemy/RotateWreckage.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RotateWreckage : MonoBehaviour
+{
+	[SerializeField]Transform transformToRotateWith;
+
+	void OnEnable()
+	{
+		transform.rotation = Quaternion.Euler(-90, 0, 0) * transformToRotateWith.rotation;
+	}
+}
