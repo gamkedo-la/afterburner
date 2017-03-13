72126e21f6bb3cea252e00a932a48ae3a92c3904
NickPolchies <polchiesnicholas@gmail.com>
2017-03-12 23:08:50 -0300

    Future turret aim, crystal alien blinks, new enemy tuning
    Future turret adjusts aim down to compensate for pivot location
    Crystal alien now charred black when dead
    Crystal alien blinks before it explodes
    Crystal chaser should run into player less
    General tuning of crystal chaser/enemy and general fixes

--- Assets/EnemyGunTurretAiInputFuture.cs
+++ Assets/EnemyGunTurretAiInputFuture.cs
@@ -1,32 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 [RequireComponent(typeof(GunTurretControl))]
+public class EnemyGunTurretAiInputFuture : EnemyGunTurretAiInput
+{
+	[SerializeField]
+	float firingOffset;
+
+	protected override void UpdateAttack()
+	{
+		Vector3 targetDirection = m_playerDirection;
+
+		if(useNewFiringSolution)
+		{
+			targetDirection = EnemyAIUtils.calculateLeadDirection(m_playerDirection, bulletSpeed, m_decisionRate);
+			targetDirection += m_gunBarrelTransform.up * firingOffset;
+			if(targetDirection == Vector3.zero)
+			{
+				targetDirection = m_playerDirection;
+			}
+		}
+
+		var forwardOnGround = new Vector2(m_gunBarrelTransform.forward.x, m_gunBarrelTransform.forward.z);
+		var playerDirectionOnGround = new Vector2(targetDirection.x, targetDirection.z);
+		var playerDirectionVertical = new Vector2(playerDirectionOnGround.magnitude, targetDirection.y);
+
+		float angleForwards = Mathf.Atan2(forwardOnGround.x, forwardOnGround.y);
+		float anglePlayer = Mathf.Atan2(playerDirectionOnGround.x, playerDirectionOnGround.y);
+
+		m_playerHorizontalAngle = EnemyAIUtils.StandardiseAngle(Mathf.Rad2Deg * (anglePlayer - angleForwards));
+
+		float playerVerticalAngle = Mathf.Atan2(playerDirectionVertical.y, playerDirectionVertical.x);
+		float gunBarrelVerticalAngle = Mathf.Atan2(m_gunBarrelTransform.forward.y, forwardOnGround.magnitude);
+
+		m_playerVerticalAngle = Mathf.Rad2Deg * (gunBarrelVerticalAngle - playerVerticalAngle);
+
+		Vector2 gunOrientation = m_gunTurretControlScript.getOrientation();
+
+		float maxPitchPerDecision = m_gunTurretControlScript.getPitchRate() * m_decisionRate;
+		float maxTurnPerDecision = m_gunTurretControlScript.getTurnRate() * m_decisionRate;
+
+		m_v = m_playerVerticalAngle / maxPitchPerDecision;
+		m_h = m_playerHorizontalAngle / maxTurnPerDecision;
+
+		//Debug.DrawLine(m_gunBarrelTransform.position, targetDirection + m_gunBarrelTransform.position, Color.red, 0.2f);
+
+	}
+}
+/*
 public class EnemyGunTurretAiInput : MonoBehaviour
 {
 	[SerializeField]
-	protected Transform m_gunBarrelTransform;
+	Transform m_gunBarrelTransform;
 	[SerializeField]
-	protected float m_decisionRate = 0.2f;
+	float m_decisionRate = 0.2f;
 	[SerializeField]
 	float m_playerInRangeAttackThreshold = 500f;
 	[SerializeField]
-	protected bool useNewFiringSolution = true;
+	bool useNewFiringSolution = true;
 
-	protected GunTurretControl m_gunTurretControlScript;
+	private GunTurretControl m_gunTurretControlScript;
 	private WaitForSeconds m_waitTime;
 	private Transform m_player;
 
 	private State m_state = State.Rest;
-	protected Vector3 m_playerDirection;
+	private Vector3 m_playerDirection;
 
-	protected float m_playerVerticalAngle;
-	protected float m_playerHorizontalAngle;
+	private float m_playerVerticalAngle;
+	private float m_playerHorizontalAngle;
 
-	protected float m_v;
-	protected float m_h;
+	private float m_v;
+	private float m_h;
 
-	protected float bulletSpeed;
+	private float bulletSpeed;
 
 	void Awake()
 	{
@@ -110,17 +157,17 @@ public class EnemyGunTurretAiInput : MonoBehaviour
 	}
 
 
-	protected virtual void UpdateAttack()
+	private void UpdateAttack()
 	{
 		Vector3 targetDirection = m_playerDirection;
 
-    if(useNewFiringSolution)
+		if(useNewFiringSolution)
 		{
 			targetDirection = EnemyAIUtils.calculateLeadDirection(m_playerDirection, bulletSpeed, m_decisionRate);
 			if(targetDirection == Vector3.zero)
 			{
 				targetDirection = m_playerDirection;
-      }
+			}
 		}
 
 		var forwardOnGround = new Vector2(m_gunBarrelTransform.forward.x, m_gunBarrelTransform.forward.z);
@@ -138,14 +185,14 @@ public class EnemyGunTurretAiInput : MonoBehaviour
 		m_playerVerticalAngle = Mathf.Rad2Deg * (gunBarrelVerticalAngle - playerVerticalAngle);
 
 		Vector2 gunOrientation = m_gunTurretControlScript.getOrientation();
-		
+
 		float maxPitchPerDecision = m_gunTurretControlScript.getPitchRate() * m_decisionRate;
 		float maxTurnPerDecision = m_gunTurretControlScript.getTurnRate() * m_decisionRate;
 
 		m_v = m_playerVerticalAngle / maxPitchPerDecision;
 		m_h = m_playerHorizontalAngle / maxTurnPerDecision;
 
-		Debug.DrawLine(m_gunBarrelTransform.position, targetDirection + m_gunBarrelTransform.position, Color.red, 0.2f);
+		//Debug.DrawLine(m_gunBarrelTransform.position, targetDirection + m_gunBarrelTransform.position, Color.red, 0.2f);
 	}
 
 	private enum State
@@ -154,3 +201,4 @@ public class EnemyGunTurretAiInput : MonoBehaviour
 		Attack = 1,
 	}
 }
+*/
--- Assets/Scripts/Enemy/SelfDestructTime.cs
+++ Assets/Scripts/Enemy/SelfDestructTime.cs
@@ -4,14 +4,54 @@ using System.Collections;
 public class SelfDestructTime : MonoBehaviour {
 	EnemyHealth myHealth;
 
+	[SerializeField]
+	GameObject model;
+	private Material crystalAlienMaterial;
+	float explosionTimer, flashRate;
+
+	[SerializeField]
+	float flashDuration, flashRateDelta, flashRateAcceleration;
+	//Color color;
+
 	// Use this for initialization
 	void Start () {
 		myHealth = GetComponent<EnemyHealth>();
+		explosionTimer = Random.Range(20f, 25.0f);
+		//explosionTimer = Random.Range(10.0f, 20.0f);
+
+		crystalAlienMaterial = model.GetComponent<Renderer>().material;
+
 		StartCoroutine(DestroySelfOnDelay());
+		flashRate = 2.7f;
+		//		color = Color.white;
 	}
-	
+
+	void Update()
+	{
+		explosionTimer -= Time.deltaTime;
+		if(explosionTimer < flashDuration)
+		{
+			flashRate = Mathf.Repeat((flashRate + 0.05f * flashRateDelta), 2);
+			flashRateDelta += flashRateAcceleration;
+			flashRateAcceleration += flashRateAcceleration * 0.01f;
+			Debug.Log(flashRate);
+
+			if(flashRate <= 1)
+			{
+				crystalAlienMaterial.SetColor("_Color", Color.Lerp(Color.red, Color.white, flashRate));
+			}
+			else
+			{
+				crystalAlienMaterial.SetColor("_Color", Color.Lerp(Color.red, Color.white, 2 - flashRate));
+			}
+
+
+		}
+	}
+
 	IEnumerator DestroySelfOnDelay() {
-		yield return new WaitForSeconds( Random.Range(10.0f, 20.0f) );
+		yield return new WaitForSeconds( explosionTimer );
 		myHealth.Damage(999999);
+		this.enabled = false;
 	}
 }
